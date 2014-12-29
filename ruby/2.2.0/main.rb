7c0bafbf5cb8e1bb9acf0ffb33873bdd94a58949
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2014-12-29 11:51:15 -0200

    Created recipe for Ruby 2.2.0.

---
+++ ruby/2.2.0/main.rb
@@ -0,0 +1,22 @@
+class Ruby220 < FPM::Cookery::Recipe
+  description 'The Ruby virtual machine'
+  name        'ruby'
+  version     '1:2.2.0'
+  revision    0
+  homepage    'http://www.ruby-lang.org/'
+  source      'http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.bz2'
+  sha256      '1c031137999f832f86be366a71155113675b72420830ce432b777a0ff4942955'
+  section     'interpreters'
+
+  build_depends 'autoconf', 'readline-devel', 'bison', 'zlib-devel', 'openssl-devel', 'libyaml-devel'
+
+  def build
+    configure :prefix => prefix, 'disable-install-doc' => true
+    make
+  end
+
+  def install
+    make :install, 'DESTDIR' => destdir
+  end
+end
+
