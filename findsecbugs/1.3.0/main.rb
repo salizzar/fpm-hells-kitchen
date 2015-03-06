7c92b213e3bfd74b5719a2b62cdd12267413a122
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-03-06 16:30:19 -0300

    Created recipe for FindSecBugs.

---
+++ findsecbugs/1.3.0/main.rb
@@ -0,0 +1,24 @@
+class FindSecBugs301 < FPM::Cookery::Recipe
+  name          'findbugs-findsecbugs-plugin'
+  version       '3.0.1'
+  revision      0
+  description   'Find Security Bugs plugin for findbugs'
+  homepage      'http://findbugs.sourceforge.net'
+  section       'development'
+  arch          'noarch'
+
+  source        'https://repo1.maven.org/maven2/com/h3xstream/findsecbugs/findsecbugs-plugin/1.3.0/findsecbugs-plugin-1.3.0.jar'
+  md5           '842db3ae425a4c40a963e0d8acf3c758'
+
+  depends       'findbugs'
+
+  def build
+  end
+
+  def install
+    path = File.join(builddir, '*/*.jar')
+
+    Dir[path].each { |jar| opt('findbugs/plugin').install jar }
+  end
+end
+
