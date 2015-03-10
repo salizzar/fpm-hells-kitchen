ae6510f59d0c5d2a79d2add5e4cb39d2b4f452fb
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-03-10 20:17:59 -0300

    Updated consul 0.4.1 recipe.

--- consul/0.4.1/main.rb
+++ consul/0.4.1/main.rb
@@ -2,12 +2,14 @@ class Consul041 < FPM::Cookery::Recipe
   name        'consul'
   version     '0.4.1'
   revision    0
-  description 'A Service discovery and configuration made easy'
+  description 'A service discovery and configuration made easy.'
   homepage    'http://consul.io'
   section     'admin'
 
   source      "https://dl.bintray.com/mitchellh/consul/#{version}_linux_amd64.zip"
   sha256      "2cf6e59edf348c3094c721eb77436e8c789afa2c35e6e3123a804edfeb1744ac"
+
+  build_depends 'zip'
 
   def build
   end
